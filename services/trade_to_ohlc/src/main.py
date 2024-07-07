5ddf73d1d49882aa0cb64ec0d57af991b18ac6ab
aparnadotk <aparnadotk@gmail.com>
2024-07-07 23:06:29 +0530

    Basic trade_to_ohlc method

---
+++ services/trade_to_ohlc/src/main.py
@@ -0,0 +1,8 @@
+def trade_to_ohlc(
+    kafka_input_topic: str,
+    kafka_output_topic: str,
+    kafka_broker_address: str,
+    kafka_consumer_group: str,
+    ohlc_window_seconds: int,
+) -> None:
+    pass
